03ff93a3970218ebd46a43d617c9ae6c941089b8
BogdanVeligorskyi <bogdan.veligorskyi@gmail.com>
2021-04-28 21:34:08 +0300

    Improved code of CutTailSpaces and CopyStrings procedures

--- TailSpacesRemover.asm
+++ TailSpacesRemover.asm
@@ -5,7 +5,6 @@ StdOut proto :DWORD
 StdIn proto :DWORD, :DWORD
 CutTailSpaces proto :DWORD
 CopyStrings proto: DWORD
-PreparingProc proto: DWORD
 
 ; db (1 Byte) = BYTE
 ; dw (2 Bytes) = WORD
@@ -15,7 +14,6 @@ PreparingProc proto: DWORD
 introString db "Tail Spaces Remover. 2021. Veligorskyi B.O., KI-191.", 0
 enterString db "Введiть рядок (string1): ", 0
 noTailString db "Рядок бех хвостових пробiлiв (string2): ", 0
-zeroByte db "0-байт", 0
 Crlf db 13, 10, 0
 enteredBytes dd ?
 maxLength dd 78
@@ -29,7 +27,7 @@ invoke StdOut, ADDR introString			;?нформац?я про розробника
 invoke StdOut, ADDR Crlf				;перех?д на новий рядок
 invoke StdOut, ADDR enterString			;"Введ?ть рядок:"
 invoke StdIn, ADDR string1, maxLength	;введення рядка користувачем
-invoke PreparingProc, ADDR string1		;
+;invoke PreparingProc, ADDR string1		;
 invoke CutTailSpaces, ADDR string1		;вилучення хвостових проб?л?в з рядка
 invoke CopyStrings, ADDR string1		;коп?ювання рядка
 invoke StdOut, ADDR noTailString		;"Рядок без хвостових проб?л?в:"
@@ -39,6 +37,9 @@ invoke ExitProcess, 0
 
 ;процедура CutTailSpaces - вилучення хвостових проб?л?в з введеного рядка
 CutTailSpaces PROC sz :DWORD
+	lea EBX, string1
+	mov byte ptr [EBX+EAX], 0
+	mov enteredBytes, EAX
 	mov ECX, enteredBytes
 	JCXZ stop
 	@@:
@@ -48,7 +49,7 @@ CutTailSpaces PROC sz :DWORD
 		m1: inc EBX
 	loopne @B
 	stop:
-	mov byte ptr [EBX+1], 0
+	mov byte ptr [EDX+1], 0
 	ret
 CutTailSpaces endp
 
@@ -58,24 +59,16 @@ CopyStrings PROC sz :DWORD
 	sub EDX, EBX
 	mov ECX, EDX ;параметр циклу
 	lea EDX, string2
+	mov byte ptr [EDX+ECX+1], 0
 	JCXZ stop1
 	@@:
-		dec ECX
-		mov AX, [EBX+ECX]
-		mov word ptr [EDX+ECX], AX
+		mov AH, [EBX+ECX]
+		mov byte ptr [EDX+ECX], AH
 		cmp ECX, 0
-	jnz @B
+		dec ECX
+		jnl @B
 	stop1:
 	ret
 CopyStrings endp
-
-PreparingProc PROC sz:DWORD
-	lea EBX, string1
-	mov byte ptr [EBX+EAX], 13
-	mov byte ptr [EBX+EAX+1], 10
-	mov byte ptr [EBX+EAX+2], 0
-	mov enteredBytes, EAX
-	ret
-PreparingProc endp
 
 end start
